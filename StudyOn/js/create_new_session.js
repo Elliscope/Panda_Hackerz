71cf6b2085c18e3c713c9a0c4d3663fd590eb2cc
justindcheng <hochingc@usc.edu>
2014-10-26 08:43:23 -0700

    Added Create Functionality

--- StudyOn/js/create_new_session.js
+++ StudyOn/js/create_new_session.js
@@ -19,7 +19,27 @@ var main=function()
    {
        $('.NewSession').hide();
                
-   });   
+   });
+   
+   $('.create_option').click(function()
+   {
+       var name = "John Appleseed";
+       var topic = document.createnew.topic.value;
+       var place = document.createnew.place.value;
+       var endTimes = document.createnew.endTime.value;
+       var endTime = new Date(endTimes)
+ 
+       var groupTable = Parse.Object.extend("Groups");
+       var agroup = new groupTable();
+       //agroup.save({Title: "title", Creator: "aq", Place: "somewhere"}).then(function(object) {
+       // alert("created!!!");
+       // });
+       agroup.save({Title: topic, Creator: name, Place: place, EndTime: endTime}).then(function(object) {
+          alert("The group has been created.");
+       });
+       $('.NewSession').hide();
+               
+   });
 
     $('#class1').click(function()
    {
